e26a4cf3bcee7a75a8858ea54269ba66f7f72c67
Kuldeep Pisda <pisdak79@gmail.com>
2017-09-26 18:02:05 +0530

    something

--- login_register/views.py
+++ login_register/views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
 import json
 
 
@@ -9,5 +10,5 @@ import json
 def UserProfileCreationAPIView(request):
 	if request.method == 'POST':
 		print request.POST
-
+		return JsonResponse(request.POST, safe=False)
 
