facf5f08a563fd88dbdfd1cac28f2871051d0bd1
shreyanshs7 <shreyanshss7@gmail.com>
2017-09-28 11:59:00 +0530

    Updated SOS app

--- Dispensary/settings.py
+++ Dispensary/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login_register',
+    'SOS',
+    'OTP',
 ]
 
 MIDDLEWARE_CLASSES = [
--- SOS/models.py
+++ SOS/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class SOS_Detail(models.Model):
+	user = models.CharField(max_length=120)
+	number1 = models.IntegerField()
+	number2 = models.IntegerField()
+	number3 = models.IntegerField()
