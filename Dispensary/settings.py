7688e764f191efb41adf3780bb74c9b30c7e8d66
shreyanshs7 <shreyanshss7@gmail.com>
2018-03-02 13:47:24 +0530

    Added hosting files for heroku

--- Dispensary/settings.py
+++ Dispensary/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Dispensary.wsgi --log-file -
