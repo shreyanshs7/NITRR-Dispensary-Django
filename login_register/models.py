60b6780095c7cfa84c3dcf5f0ecc08cf0f3e6a36
shreyanshs7 <shreyanshss7@gmail.com>
2019-10-26 18:56:15 +0530

    Removed personal email as model default

--- login_register/models.py
+++ login_register/models.py
@@ -11,7 +11,7 @@ class UserDetail(models.Model):
 	username = models.CharField(max_length=120)
 	mobile_number = models.IntegerField()
 	blood = models.CharField(max_length=12,default="A+")
-	email = models.EmailField(default="shreyanshss7@gmail.com")
+	email = models.EmailField(default="")
 
 	def __str__(self):
 		return self.username
